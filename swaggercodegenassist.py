448bbc234d510e43dc9254e3f313503e4e467981
blackboarddd <blackboarddd@outlook.com>
2017-12-27 18:06:35 +0800

    Update swaggercodegenassist.py

--- swaggercodegenassist.py
+++ swaggercodegenassist.py
@@ -9,6 +9,6 @@ import sys
 def runcommand(input, output):
     str = "java -jar swagger-codegen-cli.jar generate -l java -i " + input + " -o " + output + " -Dapis -Dmodels --model-package com.microstrategy.yatiactions.rest.model --api-package com.microstrategy.yatiactions.rest.api "
     p = os.popen(str)
-    print(p)
+    print("Generation is Finished!")
     
 runcommand(sys.argv[1], sys.argv[2])
